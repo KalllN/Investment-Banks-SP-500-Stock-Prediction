b28c29112f8b36e7823ff5df090c78356b9a8940
Kal <49631121+KalllN@users.noreply.github.com>
2021-08-12 09:11:05 +0530

    moving average

---
+++ data/sma.py
@@ -0,0 +1,13 @@
+sma30 = []
+sma100 = []
+
+for i, bank in enumerate(ib_bank_list, 1):
+    plt.subplot(2, 2, i)
+    
+    avg30day = pd.DataFrame()
+    avg30day['close'] = bank['close'].rolling(window = 30).mean()
+    sma30.append(avg30day)
+    
+    avg100day = pd.DataFrame()
+    avg100day['close'] = bank['close'].rolling(window = 100).mean()
+    sma100.append(avg100day)
