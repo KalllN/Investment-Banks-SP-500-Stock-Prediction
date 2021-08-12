b777377794c3b9dc39f6cf3cb55cac4970736068
Kal <49631121+KalllN@users.noreply.github.com>
2021-08-12 09:13:50 +0530

    Create return.py

---
+++ data/return.py
@@ -0,0 +1,13 @@
+#Daily Return
+plt.figure(figsize = (20, 13))
+for i, bank in enumerate(ib_bank_list, 1):
+    plt.subplot(2, 2, i)
+    bank['Daily Return'] = bank['close'].pct_change()
+    sns.distplot(bank['Daily Return'].dropna(),bins = 2000, color = "gold")
+    plt.ylabel('Close Price')
+    plt.title(f"{bank_name[i - 1]}")
+
+for i, bank in enumerate(ib_bank_list, 1):
+    bank['avg30day'] = sma30[i-1]
+    bank['avg100day'] = sma100[i-1]
+    i += 1
